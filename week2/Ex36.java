b94bc6d05ae641ef8cd8c1781c1de54923615236
Keisi <keisi.polli@khk.ee>
2022-09-16 12:47:52 +0300

    Week 2 - Ex 36.3

--- week2/Ex36.java
+++ week2/Ex36.java
@@ -6,14 +6,17 @@ public class Ex36 {
         Scanner reader = new Scanner(System.in);
         int number;
         int sum = 0;
+        int count = 0;
         while (true) {
             number = Integer.parseInt(reader.nextLine());
             if (number == -1) {
                 break;
             }
             sum += number; // sum = sum + number;
+            count++;
         }
         System.out.println("Thank you and see you later!");
         System.out.println("The sum is " + sum);
+        System.out.println("How many numbers: " + count);
     }
 }
