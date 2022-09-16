98b18a7117b2f6050c3145970cc32ef7373f48a4
Keisi <keisi.polli@khk.ee>
2022-09-16 12:46:54 +0300

    Week 2 - Ex 36.2

--- week2/Ex36.java
+++ week2/Ex36.java
@@ -1,18 +1,19 @@
 package week2;
-
 import java.util.Scanner;
-
 public class Ex36 {
     public static void main(String[] args) {
         System.out.println("Type number: ");
         Scanner reader = new Scanner(System.in);
         int number;
+        int sum = 0;
         while (true) {
             number = Integer.parseInt(reader.nextLine());
             if (number == -1) {
                 break;
             }
+            sum += number; // sum = sum + number;
         }
         System.out.println("Thank you and see you later!");
+        System.out.println("The sum is " + sum);
     }
 }
