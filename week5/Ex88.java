6439a5e7574d8e61514015054cf78fb712eae2e4
Keisi <keisi.polli@khk.ee>
2022-10-10 13:19:23 +0300

    Week 5 - Ex 88.1

---
+++ week5/Ex88.java
@@ -0,0 +1,10 @@
+package week5;
+
+public class Ex88 {
+    public static void main(String[] args) {
+        Student pekka = new Student("Pekka Mikkola", "013141590");
+        System.out.println("name: " + pekka.getName());
+        System.out.println("studentnumber: " + pekka.getStudentNumber());
+        System.out.println(pekka);
+    }
+}
