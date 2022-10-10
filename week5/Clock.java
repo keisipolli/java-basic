d1e7d8617084c3d9b28db49c2fecd4aa728b267a
Keisi <keisi.polli@khk.ee>
2022-10-10 13:05:14 +0300

    Week 5 - Ex 89

---
+++ week5/Clock.java
@@ -0,0 +1,33 @@
+package week5;
+
+public class Clock {
+    private BoundedCounter hours;
+    private BoundedCounter minutes;
+    private BoundedCounter seconds;
+
+    public Clock(int hoursAtBeginning, int minutesAtBeginning, int secondsAtBeginning) {
+        this.hours = new BoundedCounter(23);
+        this.minutes = new BoundedCounter(59);
+        this.seconds = new BoundedCounter(59);
+
+        this.hours.setValue(hoursAtBeginning);
+        this.minutes.setValue(minutesAtBeginning);
+        this.seconds.setValue(secondsAtBeginning);
+
+    }
+
+    public void tick() {
+        this.seconds.next();
+        if (this.seconds.getValue() == 0) {
+            this.minutes.next();
+            if (this.minutes.getValue() == 0) {
+                this.hours.next();
+            }
+        }
+    }
+
+    public String toString() {
+        return this.hours + ":" + this.minutes + ":" + this.seconds;
+    }
+}
+
