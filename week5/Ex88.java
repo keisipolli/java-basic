1e1db59a3affafcaca4391b62e04b956df155d1c
Keisi <keisi.polli@khk.ee>
2022-10-10 13:21:58 +0300

    Week 5 - Ex 88.2/3

--- week5/Ex88.java
+++ week5/Ex88.java
@@ -1,10 +1,36 @@
 package week5;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Ex88 {
     public static void main(String[] args) {
-        Student pekka = new Student("Pekka Mikkola", "013141590");
-        System.out.println("name: " + pekka.getName());
-        System.out.println("studentnumber: " + pekka.getStudentNumber());
-        System.out.println(pekka);
+        ArrayList<Student> list = new ArrayList<Student>();
+        Scanner reader = new Scanner(System.in);
+        while (true) {
+            System.out.print("name: ");
+            String name = reader.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+            System.out.print("studentnumber: ");
+            String number = reader.nextLine();
+            list.add(new Student(name, number));
+        }
+
+        for (Student student : list) {
+            System.out.println(student);
+        }
+
+        System.out.println();
+
+        System.out.print("Give search term: ");
+        String searchTerm = reader.nextLine();
+        System.out.println("Result:");
+
+        for (Student student : list) {
+            if (student.getName().contains(searchTerm)) {
+                System.out.println(student);
+            }
+        }
     }
 }
