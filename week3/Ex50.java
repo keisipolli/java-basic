122b72a680063c14315c72071361546d1ada850f
Keisi <keisi.polli@khk.ee>
2022-10-10 12:46:31 +0300

    Week 3 - Ex 50

---
+++ week3/Ex50.java
@@ -0,0 +1,20 @@
+package week3;
+
+import java.util.Scanner;
+
+public class Ex50 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type your name: ");
+        String text = reader.nextLine();
+        int i = 0;
+
+        if (text.length() > 2) {
+            while (i < 3) {
+                System.out.println( (i + 1) + ". character: " + text.charAt(i));
+                i++;
+            }
+        }
+    }
+}
